094e80bdafbd690ccf9746dbd356df49c517d19f
Vinicius <viniciusbsouto@gmail.com>
2025-09-29 21:38:45 -0300

    chore: add vercel config file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const jsonServer = require('json-server');
+const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+// Resolve db.json relative to this file so the server works regardless of CWD
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -14,4 +16,4 @@ server.listen(3000, () => {
   console.log('JSON Server is running on port 3000');
 });
 
-modules.exports = server;
+module.exports = server;
